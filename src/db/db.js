145e84de958e9e7b5f4d0d80ea3ec38920e553b0
maharanakiran60-bit <maharanakiran60@gmail.com>
2026-08-11 19:26:28 +0530

    mongodb connected

---
+++ src/db/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_Name } from "../constants.js";
+
+const connectDB = async () => {
+    try {
+        const connectionInstance = await mongoose.connect(
+            `${process.env.MONGODB_URI}/${DB_Name}`
+        );
+        console.log(
+            `\nMongoDB Connected !! DB HOST: ${connectionInstance.connection.host}`
+        );
+    } catch (error) {
+        console.log("DB connection error", error);
+        process.exit(1);
+    }
+};
+
+export default connectDB;
